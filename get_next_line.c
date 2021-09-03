945607788ccc469e502d43bd27920916afe6b404
Hector <hectorhu17@gmail.com>
2021-09-03 14:00:11 -0300

    remove unused func

--- get_next_line.c
+++ get_next_line.c
@@ -6,50 +6,18 @@
 /*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/17 10:34:08 by hectfern          #+#    #+#             */
-/*   Updated: 2021/09/03 12:50:18 by hectfern         ###   ########.fr       */
+/*   Updated: 2021/09/03 13:59:25 by hectfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-char	*ft_strncpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int	count;
-
-	count = 0;
-	while (count < n && src[count] != '\0')
-	{
-		dest[count] = src[count];
-		count++;
-	}
-	while (count < n)
-	{
-		dest[count] = '\0';
-		count++;
-	}
-	return (dest);
-}
-
-char	*ft_strcpy(char *dest, char *src)
-{
-	int		i;
-
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
 
 static char	*check_eof(int bytes_read, char *line)
 {
 	if (!bytes_read && line[bytes_read] == '\0')
 	{
 		free(line);
-		return (0);
+		return (NULL);
 	}
 	return (line);
 
@@ -89,16 +57,16 @@ static char	*get_line(int fd, char *line, char **buffer)
 
 char	*get_next_line(int fd)
 {
-	static char	*buff;
+	static char	*backup;
 	char		*line;
 
 	if (fd < 0 || BUFFER_SIZE < 1)
 		return (NULL);
-	if (!buff)
-		buff = ft_strdup("");
-	line = ft_strdup(buff);
-	free(buff);
-	buff = NULL;
-	line = get_line(fd, line, &buff);
+	if (!backup)
+		backup = ft_strdup("");
+	line = ft_strdup(backup);
+	free(backup);
+	backup = NULL;
+	line = get_line(fd, line, &backup);
 	return (line);
 }
